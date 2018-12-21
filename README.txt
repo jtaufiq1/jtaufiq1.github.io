94798df9de395eb0366431c2d0c8766488567018
Taufiq Gh <jtaufiq1@gmail.com>
2018-12-21 00:23:36 +0000

    Objectives section

---
+++ README.txt
@@ -0,0 +1,68 @@
+As part of our care and admiration,
+we will like to help you learn some
+of the basic IT/ICT content as an IT student
+
+If you will pay heed to the reading
+items that we will send to you,
+IN SHA ALLAH, you'll have fair
+knowledge in the related fields
+that will keep you going in the IT world
+
+1. An Overview of ICT
+(Recap of what you already know)
+* Computer hardware and Software
+* How the computer works
+* Overview of computer networking
+* How the internet works
+
+2. Intro to web
+* Mackup Languages (HTML, XML)
+
+3. Developing Essential [de_01]
+* Version Control System (VCS) - git and github
+
+4. Intro to programming
+* MIT Scratch
+* Algorithm and flow chart [de_01_5]
+* Intro C/C++
+* Intro OOP
+
+** Database management system **
+
+5. Developing Essential [de_02]
+* Algorithm and Data structures
+
+7. Intro to Virtualization
+8. Intro to Unix/Linux OS
+9. Introd to Pentesting and computer forensics
+
+*** Online CS Courses Platforms ***
+
+#NB: 
+	Based on this, you will find
+additional areas that might need
+exploring or Some others that are
+related to them but not in this list
+
+![IMPORTANT]:
+	Essentials for developing are applicable to almost all what you'll learn.
+	[VCS, Algorithms, Data Structures, Refactoring, Design Patterns]
+	
+@(Option):
+	You can learn more on the
+field that you find interesting if
+you wish to continue
+
+		
+@(Optional):
+	You'll learn how to search
+for information on the internet
+
+@(Optional):
+	Google, Wikipedia, Stack
+Overflow and other websites will become your friends to.
+
+@(Optional):
+	If You learn well and adapt the
+Unix way, you might understand some
+of my statements you find to be strange.
---
+++ main.js
@@ -0,0 +1,58 @@
+
+// Collapsible Function
+function collpasible() {
+    // List of Collapsible content
+    var coll = document.getElementsByClassName('collapsible');
+
+    // Iterate over content
+    for(let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function(){
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            // Display content
+            if(content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = none;
+            }
+
+            // Add animation
+            if(content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+}
+
+/**
+ * 
+ * @param {*} name 
+ * class of collapsible content
+ */
+function collpasible(name) {
+    // List of Collapsible content
+    var coll = document.getElementsByClassName(name);
+
+    // Iterate over content
+    for(let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function(){
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            // Display content
+            if(content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = none;
+            }
+
+            // Add animation
+            if(content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+}
